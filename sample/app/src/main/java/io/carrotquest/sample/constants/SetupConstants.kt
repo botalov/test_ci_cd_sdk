2ccd755bf3fe6b84930a18a15cbb68f14a76c062
Nikita <botalovnikita@yandex.ru>
2022-05-12 11:18:01 +0500

    First commit

---
+++ sample/app/src/main/java/io/carrotquest/sample/constants/SetupConstants.kt
@@ -0,0 +1,16 @@
+package io.carrotquest.sample.constants
+
+/**
+ * Добавьте сюда свои данные для авторизации. Их можно найти на странице для разработчиков -
+ * https://carrotquest.io/panel/XYZ/settings/developers, где XYZ - идентификатор вашего аппа
+ *
+ * Также эти данные можно указать и при первом запуске данного демонстрационного приложения
+ */
+
+//const val API_KEY = ""
+//const val APP_ID = ""
+//const val USER_AUTH_KEY = ""
+
+const val APP_ID = "3060"
+const val API_KEY = "3060-ed66f34a99b114d5aa7161b3619"
+const val USER_AUTH_KEY = "userauthkey-3060-2fd1047cb540b573999a9d101a9e7f54a50cf54d380bea175b1c9adf95b6252"
